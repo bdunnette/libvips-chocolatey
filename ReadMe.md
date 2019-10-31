856b7b8f8ad823c1c92f9dd089b4100dd1dfa199
Brian Dunnette <dunn0172@umn.edu>
2019-10-31 09:08:47 -0500

    bump to 8.8.3, update readme

--- ReadMe.md
+++ ReadMe.md
@@ -1,1 +1,14 @@
 # libvips-chocolatey
+
+## Updating
+
+1. Find the latest released version: https://github.com/libvips/libvips/releases
+
+2. Update version number in **libvips.nuspec**:
+        
+        <version>8.8.3</version>
+
+3. Rebuild and install the package:
+   
+        choco pack
+        choco upgrade libvips -dv -s .
--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -6,7 +6,7 @@
 # 2. Follow the documentation below to learn how to create a package for the package type you are creating.
 # 3. In Chocolatey scripts, ALWAYS use absolute paths - $toolsDir gets you to the package's tools directory.
 $ErrorActionPreference = 'Stop'; # stop on all errors
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 # Internal packages (organizations) or software that has redistribution rights (community repo)
 # - Use `Install-ChocolateyInstallPackage` instead of `Install-ChocolateyPackage`
 #   and put the binaries directly into the tools folder (we call it embedding)
@@ -15,45 +15,45 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 #$fileLocation = '\\SHARE_LOCATION\to\INSTALLER_FILE'
 # Community Repo: Use official urls for non-redist binaries or redist where total package size is over 200MB
 # Internal/Organization: Download from internal location (internet sources are unreliable)
-$url        = '' # download url, HTTPS preferred
-$url64      = 'https://github.com/libvips/libvips/releases/download/v8.8.1/vips-dev-w64-web-8.8.1a.zip' # 64bit URL here (HTTPS preferred) or remove - if installer contains both (very rare), use $url
+$url = '' # download url, HTTPS preferred
+$url64 = 'https://github.com/libvips/libvips/releases/download/v$env:chocolateyPackageVersion/vips-dev-w64-all-$env:chocolateyPackageVersion.zip' # 64bit URL here (HTTPS preferred) or remove - if installer contains both (very rare), use $url
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
-  # fileType      = 'EXE_MSI_OR_MSU' #only one of these: exe, msi, msu
-  url           = $url
-  url64bit      = $url64
-  #file         = $fileLocation
+    packageName    = $env:ChocolateyPackageName
+    unzipLocation  = $toolsDir
+    # fileType      = 'EXE_MSI_OR_MSU' #only one of these: exe, msi, msu
+    url            = $url
+    url64bit       = $url64
+    #file         = $fileLocation
 
-  softwareName  = 'libvips*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+    softwareName   = 'libvips*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
 
-  # Checksums are now required as of 0.10.0.
-  # To determine checksums, you can get that from the original site if provided. 
-  # You can also use checksum.exe (choco install checksum) and use it 
-  # e.g. checksum -t sha256 -f path\to\file
-  checksum      = ''
-  checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
-  checksum64    = ''
-  checksumType64= 'sha256' #default is checksumType
+    # Checksums are now required as of 0.10.0.
+    # To determine checksums, you can get that from the original site if provided. 
+    # You can also use checksum.exe (choco install checksum) and use it 
+    # e.g. checksum -t sha256 -f path\to\file
+    checksum       = ''
+    checksumType   = 'sha256' #default is md5, can also be sha1, sha256 or sha512
+    checksum64     = ''
+    checksumType64 = 'sha256' #default is checksumType
 
-  # MSI
-  # silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
-  # validExitCodes= @(0, 3010, 1641)
-  # OTHERS
-  # Uncomment matching EXE type (sorted by most to least common)
-  #silentArgs   = '/S'           # NSIS
-  #silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' # Inno Setup
-  #silentArgs   = '/s'           # InstallShield
-  #silentArgs   = '/s /v"/qn"'   # InstallShield with MSI
-  #silentArgs   = '/s'           # Wise InstallMaster
-  #silentArgs   = '-s'           # Squirrel
-  #silentArgs   = '-q'           # Install4j
-  #silentArgs   = '-s'           # Ghost
-  # Note that some installers, in addition to the silentArgs above, may also need assistance of AHK to achieve silence.
-  #silentArgs   = ''             # none; make silent with input macro script like AutoHotKey (AHK)
-                                 #       https://chocolatey.org/packages/autohotkey.portable
-  #validExitCodes= @(0) #please insert other valid exit codes here
+    # MSI
+    # silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
+    # validExitCodes= @(0, 3010, 1641)
+    # OTHERS
+    # Uncomment matching EXE type (sorted by most to least common)
+    #silentArgs   = '/S'           # NSIS
+    #silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' # Inno Setup
+    #silentArgs   = '/s'           # InstallShield
+    #silentArgs   = '/s /v"/qn"'   # InstallShield with MSI
+    #silentArgs   = '/s'           # Wise InstallMaster
+    #silentArgs   = '-s'           # Squirrel
+    #silentArgs   = '-q'           # Install4j
+    #silentArgs   = '-s'           # Ghost
+    # Note that some installers, in addition to the silentArgs above, may also need assistance of AHK to achieve silence.
+    #silentArgs   = ''             # none; make silent with input macro script like AutoHotKey (AHK)
+    #       https://chocolatey.org/packages/autohotkey.portable
+    #validExitCodes= @(0) #please insert other valid exit codes here
 }
 
 # Install-ChocolateyPackage @packageArgs # https://chocolatey.org/docs/helpers-install-chocolatey-package
